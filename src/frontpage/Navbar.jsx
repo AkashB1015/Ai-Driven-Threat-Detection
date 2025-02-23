c222eb368ceaa61a77e5c7dd01c129be5c111c75
AkashB3501 <akashbhadane7227@gmail.com>
2025-02-23 19:52:34 +0530

    add the new file

---
+++ src/frontpage/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./page.css";
+
+const Navbar = () => {
+    return (
+        <header>
+            <div className="container_start">
+                <nav>
+                    <div className="logo">
+                        <img src="/images/Syscure_logo.svg" className="logo-img1" alt="Syscure Logo" />
+                        Syscure
+                    </div>
+
+                    <ul className="nav-links">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#" className="cta-button">Sign Up</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+};
+
+export default Navbar;
